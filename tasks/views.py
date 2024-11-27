ecd27d2e73b4afbf38c868435c4be7248c7ef087
Constance Amatey <consamate57@gmail.com>
2024-11-27 15:58:46 +0000

    created sessions for todo list project

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -9,14 +10,17 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html", {"tasks": tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {"tasks": request.session["tasks"]})
 
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {"form": form})
     
